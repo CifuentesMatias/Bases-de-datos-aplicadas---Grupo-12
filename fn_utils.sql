fc579c26e817fa0b5db164b312c197c076f01ad4
defe <111141803+0x94DErFErNC@users.noreply.github.com>
2025-11-07 13:46:28 -0300

    Add functions for counting and summing parking/storage areas
    This SQL script creates functions to calculate the total count and area of parking spaces and storage units based on the provided consortium and unit IDs.

---
+++ fn_utils.sql
@@ -0,0 +1,52 @@
+/** COMMENTS **
+	se usa tecnica de enum como en C, para mapear una string a valor
+*/
+/** DESCRIP **
+    calcula cantidad y m2 totales de cocheras/bauleras
+    retorna 0 para ambos casos si no encuentra nada
+*/
+/** REQUIERE **
+  table Adicionales
+*/
+/** PARAMS **
+  @id_consorcio    gt(1)                -
+  @id_uf           gte(1)               - 
+*/
+
+CREATE FUNCTION fn_CANT_COCHERAS(@id_consorcio INT, @id_uf INT) RETURNS INT AS
+BEGIN
+	DECLARE @id_cochera INT = (SELECT id_tipo FROM tipo_adicional WHERE descripcion = 'Cochera');
+
+	RETURN (SELECT COALESCE(COUNT(*), 0) 
+			    FROM adicionales a 
+			    WHERE id_consorcio = @id_consorcio AND id_uf = @id_uf AND a.tipo = @id_cochera);
+END
+
+CREATE FUNCTION fn_M2TOTAL_COCHERAS(@id_consorcio INT, @id_uf INT) RETURNS NUMERIC(5,2) AS
+BEGIN
+	DECLARE @id_cochera INT = (SELECT id_tipo FROM tipo_adicional WHERE descripcion = 'Cochera');
+
+	RETURN (SELECT COALESCE(SUM(m2), 0) 
+			    FROM adicionales a 
+			    WHERE id_consorcio = @id_consorcio AND id_uf = @id_uf AND a.tipo = @id_cochera);
+END
+
+
+  
+CREATE FUNCTION fn_CANT_BAULERAS(@id_consorcio INT, @id_uf INT) RETURNS INT AS
+BEGIN
+	DECLARE @id_baulera INT = (SELECT id_tipo FROM tipo_adicional WHERE descripcion = 'Baulera');
+
+	RETURN (SELECT COALESCE(COUNT(*), 0) 
+			    FROM adicionales a 
+			    WHERE id_consorcio = @id_consorcio AND id_uf = @id_uf AND a.tipo = @id_baulera);
+END
+
+CREATE FUNCTION fn_M2TOTAL_BAULERAS(@id_consorcio INT, @id_uf INT) RETURNS NUMERIC(5,2)  AS
+BEGIN
+	DECLARE @id_baulera INT = (SELECT id_tipo FROM tipo_adicional WHERE descripcion = 'Baulera');
+
+	RETURN (SELECT COALESCE(SUM(m2), 0) 
+			    FROM adicionales a 
+			    WHERE id_consorcio = @id_consorcio AND id_uf = @id_uf AND a.tipo = @id_baulera);
+END
