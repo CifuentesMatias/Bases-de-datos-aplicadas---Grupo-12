08514e5c5baf343bcbc4b920b5ab2622d8bd0e72
MatyBotta <68671511+MatyBotta@users.noreply.github.com>
2025-11-16 18:57:33 -0300

    Correciones en SP
    Realizado el SP de Servicios.Servicios.json
    Bien separado inquilinos_propietarios_datos de pagos_consorcios

--- Entrega 5/codigo/SP_PagosConsorcio.sql
+++ Entrega 5/codigo/SP_PagosConsorcio.sql
@@ -1,53 +1,79 @@
 use Com2900G12
+go
 
-/*
-create procedure or alter sp_ImportarPagoConsorcio
-	@RutaArchivo nvarchar(500),
-	@NombreTabla nvarchar(500),
-	@TieneEncabezado BIT = 1,
-	@Resultado INT output,
-	@Mensaje nvarchar(500) output
+sp_configure 'show advanced options', 1;
+RECONFIGURE;
+GO
+sp_configure 'Ad Hoc Distributed Queries', 1;
+RECONFIGURE;
+GO
+
+EXEC master.dbo.sp_MSset_oledb_prop 
+    N'Microsoft.ACE.OLEDB.16.0', 
+    N'AllowInProcess', 1;
+    
+EXEC master.dbo.sp_MSset_oledb_prop 
+    N'Microsoft.ACE.OLEDB.16.0', 
+    N'DynamicParameters', 1;
+
+GO
+-------- IMPORTACION DE PAGOS (pagos_consorcios.csv) --------
+create or alter procedure Pagos.sp_ImportarPagosConsorcios
+	@RutaArchivo nvarchar(500)
 as
 begin
-	set nocount on;
-
 	begin try
-		declare @sql nvarchar(max);
-		declare @PrimeraFila int = case when @TieneEncabezado = 1 then 2 else 1 end;
+		set nocount on;
 
-		-- Verificar que el archivo existe
-		declare @ArchivoExiste int;
-		exec master.dbo.xp_fileexist @RutaArchivo, @ArchivoExiste output;
-
-		if @ArchivoExiste = 0
-		begin
-			set @Resultado = 0;
-			print('El archivo no existe');
-			return;
-		end
-
-		set @sql = '
-			bulk insert ' + @NombreTabla + '
-			from ''' + @RutaArchivo + '''
-			with (
-				FIELDTERMINATOR = '','',
-                ROWTERMINATOR = ''\n'',
-                FIRSTROW = ' + CAST(@PrimeraFila AS NVARCHAR(10)) + ',
-                CODEPAGE = ''65001'',
-                TABLOCK,
-                ERRORFILE = ''C:\Logs\errores_carga.txt''
-			)';
-		
-		exec sp_executesql @sql;
-
-		set @Resultado = 1;
-		print('Archivo cargado exitosamente');
-
+        -- Tabla temporal para recibir datos en crudo (todo como VARCHAR)
+        CREATE TABLE #tmpPagoRaw(
+            id int not null,
+            fecha_pago VARCHAR(20) not null,
+            cbu_cvu VARCHAR(30) not null,
+            monto VARCHAR(20) not null
+        );
+        
+        -- Cargar archivo CSV usando BULK INSERT dinámico
+        DECLARE @SQL NVARCHAR(MAX);
+        SET @SQL = 
+        N'BULK INSERT #tmpPagoRaw
+        FROM '''+@RutaArchivo+'''
+        WITH (
+            FIRSTROW = 2,
+            FIELDTERMINATOR = '','',
+            ROWTERMINATOR = ''\n'',
+            TABLOCK,
+            CODEPAGE = ''65001''
+        );';
+        
+        PRINT @SQL;
+        EXEC sp_executesql @SQL;
+        
+        -- Insertar limpiando y transformando los datos
+        INSERT INTO Pagos.Pago(id_pago, fecha_pago, cbu_cvu, monto)
+        SELECT 
+            CAST(LTRIM(RTRIM(id)) AS INT),
+            CONVERT(DATE, LTRIM(RTRIM(fecha_pago)), 103), -- Convierte el varchar a DATE en formato dd/mm/yyyy
+            LTRIM(RTRIM(cbu_cvu)), -- Esto limpia si tiene espacios a derecha o izquierda
+            CAST(REPLACE(
+                    REPLACE(monto, '$', ''),
+                    ',', '.'
+                ) AS DECIMAL(10,3))
+        FROM #tmpPagoRaw;
+        
+        -- Mostrar los datos finales
+        SELECT * FROM Pagos.Pago;
+        
+        PRINT 'Importación completada';
+        
+        -- Limpiar tablas temporales
+        DROP TABLE #tmpPagoRaw;
 	end try
 	begin catch
-		set @Resultado = 0;
-		print('Error');
+		print 'Error en la imporacion';
+		PRINT ERROR_MESSAGE();
 	end catch
 end
-go
-*/
+
+
+exec Pagos.sp_ImportarPagosConsorcios @RutaArchivo = 'C:\Program Files\Microsoft SQL Server\MSSQL16.SQLEXPRESS\MSSQL\DATA\consorcios\pagos_consorcios.csv';
