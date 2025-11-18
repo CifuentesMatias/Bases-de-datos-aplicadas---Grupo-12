94bc38143fd53c2ac9ea9fc030a82a7a4e3ab77b
MatyBotta <68671511+MatyBotta@users.noreply.github.com>
2025-11-18 12:31:03 -0300

    Funciona la importacion de datos varios

--- Entrega 5/codigo/SP_Datos_Varios.sql
+++ Entrega 5/codigo/SP_Datos_Varios.sql
@@ -1,106 +1,133 @@
-if db_id('Com2900G12') is null
-	create database Com2900G12 collate Modern_Spanish_CI_AS;
-go
+IF DB_ID('Com2900G12') IS NULL
+    CREATE DATABASE Com2900G12 COLLATE Modern_Spanish_CI_AS;
+GO
 
-use Com2900G12
-go
+USE Com2900G12;
+GO
+
+
 
 IF OBJECT_ID('SP_DatosVarios') IS NOT NULL
     DROP PROCEDURE SP_DatosVarios;
 GO
 
-CREATE or ALTER PROCEDURE SP_DatosVarios
-    -- Parámetros para las rutas de los archivos
-    @RutaConsorcios NVARCHAR(255) = 'C:\Temp\Consorcios\datos varios.xlsx - Consorcios.csv',
-    @RutaProveedores NVARCHAR(255) = 'C:\Temp\Consorcios\datos varios.xlsx - Proveedores.csv'
+CREATE PROCEDURE SP_DatosVarios
+    @RutaExcel NVARCHAR(500) = 'C:\Users\botta\Documents\GitHub\BaseDatosAplicadaGrupo12\Bases-de-datos-aplicadas---Grupo-12\Entrega 5\Archivos_para_importar\datos varios.xlsx'
 AS
 BEGIN
     SET NOCOUNT ON;
-
+    
     BEGIN TRY
         BEGIN TRANSACTION;
-      
-        IF OBJECT_ID('tempdb..#TempConsorcios') IS NOT NULL DROP TABLE #TempConsorcios;
+
+        
+        -- Tabla temporal para Consorcios
+        IF OBJECT_ID('tempdb..#TempConsorcios') IS NOT NULL 
+            DROP TABLE #TempConsorcios;
+            
         CREATE TABLE #TempConsorcios (
-            Id_Consorcio_Ref NVARCHAR(50), 
-            Nombre_Consorcio NVARCHAR(100), 
-            Domicilio NVARCHAR(255), 
+            ID_Consorcio varchar(30), 
+            Nombre_Consorcio VARCHAR(50), 
+            Domicilio VARCHAR(50), 
             Cant_Unidades_Funcionales INT, 
             M2_Totales DECIMAL(10, 2)
         );
         
+        -- Importar Consorcios desde Excel
         DECLARE @SQLConsorcios NVARCHAR(MAX);
         SET @SQLConsorcios = '
-            BULK INSERT #TempConsorcios
-            FROM ''' + @RutaConsorcios + ''' 
-            WITH (
-                FIELDTERMINATOR = '','',     
-                ROWTERMINATOR = ''0x0a'',      
-                FIRSTROW = 2
-            );';
+            INSERT INTO #TempConsorcios (Id_Consorcio, Nombre_Consorcio, Domicilio, Cant_Unidades_Funcionales, M2_Totales)
+            SELECT 
+                *
+            FROM OPENROWSET(
+                ''Microsoft.ACE.OLEDB.12.0'',
+                ''Excel 12.0;Database=' + @RutaExcel + ';HDR=YES'',
+                ''SELECT * FROM [Consorcios$]''
+            )';
+        
         EXEC sp_executesql @SQLConsorcios;
-
-        INSERT INTO Consorcio (razon_social, direccion, m2)
+        
+        -- Insertar en tabla Consorcio
+        INSERT INTO Consorcio (razon_social, domicilio, m2)
         SELECT 
-            t.Nombre_Consorcio, t.Domicilio, t.M2_Totales
-        FROM 
-            #TempConsorcios t
-        WHERE 
-            NOT EXISTS (
-                SELECT 1 FROM Consorcio c WHERE c.razon_social = t.Nombre_Consorcio);
-
-        IF OBJECT_ID('tempdb..#TempProveedores') IS NOT NULL DROP TABLE #TempProveedores;
-        CREATE TABLE #TempProveedores (
-            Columna_Vacia NVARCHAR(255),
-            Tipo_Gasto NVARCHAR(255),
-            Nombre_Detalle NVARCHAR(255), 
-            Referencia NVARCHAR(255),    
-            Nombre_Consorcio NVARCHAR(100)
+            t.Nombre_Consorcio, 
+            t.Domicilio, 
+            t.M2_Totales
+        FROM #TempConsorcios t
+        WHERE NOT EXISTS (
+            SELECT 1 FROM Consorcio c 
+            WHERE c.razon_social = t.Nombre_Consorcio
         );
-
+        
+        PRINT 'Consorcios importados: ' + CAST(@@ROWCOUNT AS NVARCHAR(10));
+        
+        -- Tabla temporal para Proveedores
+        IF OBJECT_ID('tempdb..#TempProveedores') IS NOT NULL 
+            DROP TABLE #TempProveedores;
+            
+        CREATE TABLE #TempProveedores (
+            Tipo_Gasto NVARCHAR(50),
+            Nombre_Detalle NVARCHAR(40), 
+            Referencia NVARCHAR(40),    
+            Nombre_Consorcio NVARCHAR(50)
+        );
+        
+        -- Importar Proveedores desde Excel
         DECLARE @SQLProveedores NVARCHAR(MAX);
         SET @SQLProveedores = '
-            BULK INSERT #TempProveedores
-            FROM ''' + @RutaProveedores + ''' 
-            WITH (
-                FIELDTERMINATOR = '','',     
-                ROWTERMINATOR = ''0x0a'',      
-                FIRSTROW = 3
-            );';
+            INSERT INTO #TempProveedores (Tipo_Gasto, Nombre_Detalle, Referencia, Nombre_Consorcio)
+            SELECT 
+                *
+            FROM OPENROWSET(
+                ''Microsoft.ACE.OLEDB.12.0'',
+                ''Excel 12.0;Database=' + @RutaExcel + ';HDR=YES'',
+                ''SELECT * FROM [Proveedores$]''
+            )';
+        
         EXEC sp_executesql @SQLProveedores;
-
+        
+        -- Insertar en tabla Proveedor
         INSERT INTO Proveedor (razon_social, cuenta)
         SELECT 
-            t.Nombre_Detalle, t.Referencia
-        FROM 
-            #TempProveedores t
-        WHERE 
-            NOT EXISTS (
-                SELECT 1 FROM Proveedor p WHERE p.razon_social = t.Nombre_Detalle);
-
+            t.Nombre_Detalle, 
+            t.Referencia
+        FROM #TempProveedores t
+        WHERE t.Nombre_Detalle IS NOT NULL
+            AND NOT EXISTS (
+                SELECT 1 FROM Proveedor p 
+                WHERE p.razon_social = t.Nombre_Detalle
+            );
+        
+        -- Insertar en tabla Proveedor_Consorcio
         INSERT INTO Proveedor_Consorcio (id_consorcio, id_proveedor)
         SELECT DISTINCT 
             c.id,
             p.id
-        FROM 
-            #TempProveedores t
+        FROM #TempProveedores t
         JOIN Consorcio c ON c.razon_social = t.Nombre_Consorcio
-        JOIN Proveedor p ON p.razon_social = t.Nombre_Detalle;
+        JOIN Proveedor p ON p.razon_social = t.Nombre_Detalle
+        WHERE NOT EXISTS (
+            SELECT 1 FROM Proveedor_Consorcio pc 
+            WHERE pc.id_consorcio = c.id AND pc.id_proveedor = p.id
+        );
         
         COMMIT TRANSACTION;
-
+        
     END TRY
     BEGIN CATCH
         IF @@TRANCOUNT > 0
-        BEGIN
             ROLLBACK TRANSACTION;
-        END
         
+        DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE();
+        DECLARE @ErrorSeverity INT = ERROR_SEVERITY();
+        DECLARE @ErrorState INT = ERROR_STATE();
+        DECLARE @ErrorLine INT = ERROR_LINE();
+        
+        PRINT 'Error en línea ' + CAST(@ErrorLine AS NVARCHAR(10)) + ': ' + @ErrorMessage;
+        RAISERROR(@ErrorMessage, @ErrorSeverity, @ErrorState);
     END CATCH
-    
 END
 GO
 
-SELECT * FROM Proveedor
-
-exec dbo.SP_DatosVarios
+-- Ejecutar el procedimiento
+EXEC dbo.SP_DatosVarios;
