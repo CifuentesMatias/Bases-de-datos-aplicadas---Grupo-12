8ce2d2b17c6ab94ae25b6ac34165faa43d5219d5
defe <111141803+0x94DErFErNC@users.noreply.github.com>
2025-11-18 14:54:34 -0300

    Add stored procedure sp_reporte5 for debt report
    Create stored procedure to retrieve top 3 debtors with contact info.

---
+++ Entrega 6/SP_reporte5.sql
@@ -0,0 +1,75 @@
+/** Reporte 5 **
+	Obtenga los 3 (tres) propietarios con mayor morosidad. Presente información de contacto y
+	DNI de los propietarios para que la administración los pueda contactar o remitir el trámite
+	al estudio jurídico.
+
+	//quizas me pidan algo asi
+	CONSORCIO 		 fCalculo 	DEUDA   NOMBRE 	 		DNI 		TELEFONO 	EMAIL
+	altos del oeste  2020-1-5	50mil   Pepe jodido  	20454564  	11-23244    
+	altos del oeste  2020-1-5	47mil   Elber galarga  	20454564  				elbergalarga@yahoo.com    
+*/
+CREATE PROCEDURE sp_reporte5(@nombre_consorcio NVARCHAR(50), @fechaInicio DATE = '2020-1-1', @fechaFin DATE = NULL) AS
+BEGIN
+	SET NOCOUNT ON;
+
+	IF @fechaFin IS NULL
+		SET @fechaFin = GETDATE();
+
+	DECLARE @id_consorcio INT = (SELECT TOP 1 id_consorcio FROM consorcio WHERE razon_social = @nombre_consorcio);
+
+	IF @id_consorcio IS NULL
+	BEGIN
+		RAISERROR('Ese consorcio no existe', 16, 1);
+		RETURN;
+	END;
+	IF @fechaInicio < '2000-1-1' OR @fechaInicio > @fechaFin 
+	BEGIN
+		RAISERROR('fecha invalida', 16, 2);
+		RETURN;
+	END;
+
+
+
+	WITH
+		-- busco las uf que SOLO deban
+		uf_morosa AS (SELECT
+					   		id_uf,
+					 		saldo_final
+					   FROM 
+					 		prorateo
+					   WHERE 
+					 		id_consorcio = @id_consorcio AND
+							anio = YEAR(@fechaFin) AND mes = MONTH(@fechaFin) AND
+							saldo_final > 0),
+
+		-- me piden 3 personas morosas, entonces puede pasar que la misma persona deba dos departamentos
+		morosos AS (SELECT
+					    ur.propietario,
+					    ur.prop_dni,
+					    ur.prop_email,
+					    ur.prop_tel,
+					    SUM(ufm.saldo_final) as deuda
+					FROM 
+						fn_ultimoResidentes(@id_consorcio, @fechaFin) ur
+					JOIN 
+						uf_morosa ufm 
+						ON ufm.id_uf = ur.id_uf
+					GROUP BY 
+						ur.propietario,
+					    ur.prop_dni,
+					    ur.prop_email,
+					    ur.prop_tel) 
+	SELECT TOP 3
+		@nombre_consorcio AS consorcio,
+		@fechaFin AS fCalculo,
+		deuda,
+		propietario,
+		prop_dni,
+		prop_email,
+		prop_tel
+	FROM 
+		morosos
+	ORDER BY
+	 	deuda DESC;
+END; 
+GO
