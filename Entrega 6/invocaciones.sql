314925339759c0cbb488086a9e5c363c26245141
defe <111141803+0x94DErFErNC@users.noreply.github.com>
2025-11-18 16:35:34 -0300

    invocaciones

---
+++ Entrega 6/invocaciones.sql
@@ -0,0 +1,15 @@
+USE Com2900G12;
+GO
+
+--EXEC sp_reporte1 'Azcuenaga';
+--GO
+EXEC sp_reporte2 'Azcuenaga';
+GO
+--EXEC sp_reporte3 'Azcuenaga';
+--GO
+EXEC sp_reporte4 'Azcuenaga';
+GO
+EXEC sp_reporte5 'Azcuenaga';
+GO
+EXEC sp_reporte6 'Azcuenaga';
+GO
