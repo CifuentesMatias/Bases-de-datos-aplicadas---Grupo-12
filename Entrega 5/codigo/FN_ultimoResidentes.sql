1f8b3aea23852c2034c2820963ad0d10f33117df
defe <111141803+0x94DErFErNC@users.noreply.github.com>
2025-11-21 17:40:19 -0300

    Update de logica para manejo de casos que no existen

--- Entrega 5/codigo/FN_ultimoResidentes.sql
+++ Entrega 5/codigo/FN_ultimoResidentes.sql
@@ -1,14 +1,14 @@
 CREATE FUNCTION fn_ultimoResidentes(@id_consorcio INT, @fecha_actual DATE) RETURNS TABLE AS RETURN
 (
 	/*
-	id_uf	id_persona	es_propietario	fecha_alta	fecha_registro
-	1		100			1				2023-01-15	2023-01-10
-	1		101			1				2024-05-20	2024-05-15
-	1		200			0				2023-06-10	2023-06-05
-	1		201			0				2024-08-01	2024-07-28
-	2		300			1				2023-03-12	2023-03-10
-	2		400			0				2023-07-20	2023-07-15
-	3		500			1				2024-02-01	2024-01-25
+	id_uf	id_persona	es_propietario	fecha_alta	
+	1		100			1				2023-01-15
+	1		101			1				2024-05-20
+	1		200			0				2023-06-10
+	1		201			0				2024-08-01
+	2		300			1				2023-03-12
+	2		400			0				2023-07-20
+	3		500			1				2024-02-01
 
 
 	id_uf	id_persona	es_propietario	fecha_alta	rn
@@ -22,34 +22,43 @@ CREATE FUNCTION fn_ultimoResidentes(@id_consorcio INT, @fecha_actual DATE) RETUR
 	*/
 
     WITH
+    	Uf_porConsorcio AS (SELECT id_uf
+    						FROM unidad_funcional
+    						WHERE id_consorcio = @id_consorcio),
 		ultimo_residentes AS (SELECT
 						        id_uf,
 						        id_persona,
 						        es_propietario,
-						        ROW_NUMBER() OVER (PARTITION BY id_uf, es_propietario ORDER BY fecha_alta DESC) AS rn
-						      FROM 
-						    	persona_uf
-						      WHERE 
-						    	id_consorcio = @id_consorcio AND
-						       	fecha_alta <= @fecha_actual),
+						        fecha_alta,
+						        ROW_NUMBER() OVER (
+						        	PARTITION BY id_uf, es_propietario 
+						        	ORDER BY fecha_alta DESC
+						        ) AS rn
+							  FROM
+							    persona_uf 
+							  WHERE
+							    id_consorcio = @id_consorcio AND
+							    fecha_alta <= @fecha_actual),
 		residentes_actuales AS (SELECT
-							        id_uf,
-							        MAX(CASE WHEN es_propietario = 1 THEN id_persona END) AS id_persona_prop,
-							        MAX(CASE WHEN es_propietario = 0 THEN id_persona END) AS id_persona_inq
+							        ufc.id_uf,
+							        CASE WHEN ur.es_propietario = 1 THEN ur.id_persona ELSE NULL END AS id_persona_prop,
+							        CASE WHEN ur.es_propietario = 0 THEN ur.id_persona ELSE NULL END AS id_persona_inq
 							    FROM 
-							    	ultimo_residentes
-							    WHERE 
-							    	rn = 1
-							    GROUP BY 
-							    	id_uf)
+							    	Uf_porConsorcio ufc 
+							    LEFT JOIN
+							    	ultimo_residentes ur
+							    	ON ur.id_uf = ufc.id_uf
+							    	AND ur.rn = 1)
 	SELECT
 	    ra.id_uf,
+
 	    COALESCE(prop.nombre + ' ' + prop.apellido, '-') AS propietario,
-	    prop.dni AS prop_dni,
+	    COALESCE(CAST(prop.dni AS VARCHAR(10)), '-') AS prop_dni,
 	    COALESCE(prop.telefono, '') AS prop_tel,
 	    COALESCE(prop.email, '') AS prop_email,
+
 	    COALESCE(inq.nombre + ' ' + inq.apellido, '-') AS inquilino,
-	    inq.dni AS inq_dni,
+	    COALESCE(CAST(inq.dni AS VARCHAR(10)), '-') AS inq_dni,
 	    COALESCE(inq.telefono, '') AS inq_tel,
 	    COALESCE(inq.email, '') AS inq_email
 	FROM
